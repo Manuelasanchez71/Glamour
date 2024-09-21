52bf569d525c9bdcd380d02c66e23f40b6975e36
Manuelasanchez71 <sanchezariasmanuela213@gmail.com>
2024-09-21 15:36:15 -0500

    Agregar modelo de usuarios en src/models/ModelUsers.ts

---
+++ backend/src/models/ModelUsers.ts
@@ -0,0 +1,6 @@
+export interface IUser {
+  idUsuario?: number; // Identificador opcional del usuario
+  nombre: string;     // Nombre del usuario
+  correo: string;     // Correo electrónico del usuario
+  password: string;   // Contraseña del usuario
+}
