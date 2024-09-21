9a26d9ba05e93e4fb527cbe299bbb3885acc3e15
Manuelasanchez71 <sanchezariasmanuela213@gmail.com>
2024-09-21 15:57:45 -0500

    Actualizar .gitignore para excluir carpetas no deseadas y limpiar el seguimiento de archivos

--- backend/src/models/ModelUsers.ts
+++
@@ -1,6 +0,0 @@
-export interface IUser {
-  idUsuario?: number; // Identificador opcional del usuario
-  nombre: string;     // Nombre del usuario
-  correo: string;     // Correo electrónico del usuario
-  password: string;   // Contraseña del usuario
-}
