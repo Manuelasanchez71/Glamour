b9fafd7555c2dcdee7871644b5eeaeecbfdd732a
Manuelasanchez71 <sanchezariasmanuela213@gmail.com>
2024-09-21 15:42:28 -0500

    Agregar configuración de Tailwind CSS en tailwind.config.js

---
+++ crud/steadfast-shell/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,astro}", // Incluye todos los archivos relevantes
+    "./src/layouts/**/*.astro",
+    "./src/pages/**/*.astro",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
